742a77bb68714b1c8169fd47e7c8525dacaf5570
Peace <padelamare@gmail.com>
2021-07-13 12:43:54 +0200

    feat: deleted item in localStorage

--- front/pages/panier.js
+++ front/pages/panier.js
@@ -1,23 +1,30 @@
 let productInLocalStorage = JSON.parse(localStorage.getItem("produit"));
 
-console.log(productInLocalStorage);
+//console.log(productInLocalStorage);
 const displayArea = document.querySelector(".basket-product__container");
 
 function displayArticleInLocalStorage() {
-    for (i = 0; i < productInLocalStorage.length; i++) {
-      displayArea.innerHTML += `
+  for (i = 0; i < productInLocalStorage.length; i++) {
+    displayArea.innerHTML += `
       <div class="basket-product">
-          <div class="basket-product__name">${productInLocalStorage[i].teddyName}</div>
-          <div class="basket-product__couleur">${productInLocalStorage[i].teddyColor}</div>
-          <div class="basket-product__prix">${productInLocalStorage[i].teddyPrice/100}€</div>
-          <div class="basket-product__delete"><button class="delete" id="delete${[i]}">Supprimer l'article</button></div>
+          <div class="basket-product__name">${
+            productInLocalStorage[i].teddyName
+          }</div>
+          <div class="basket-product__couleur">${
+            productInLocalStorage[i].teddyColor
+          }</div>
+          <div class="basket-product__prix">${
+            productInLocalStorage[i].teddyPrice / 100
+          }€</div>
+          <div class="basket-product__delete"><button class="delete" id="delete${[
+            i,
+          ]}">Supprimer l'article</button></div>
         </div>`;
-    }
   }
-
+}
 
 function checkProductInLocalStorage() {
-  if (productInLocalStorage === null) {
+  if (productInLocalStorage.length === 0) {
     console.log("le panier est vide");
     displayArea.innerHTML = `<p class="empty-basket">Votre panier est vide<br/>
         <a href="../index.html" title="Revenir à l'accueil">Revenir à l'accueil</a></p>`;
@@ -27,27 +34,28 @@ function checkProductInLocalStorage() {
   }
 }
 
-
 function cart() {
-    checkProductInLocalStorage();
-    }
-    
+  checkProductInLocalStorage();
+}
+
 cart();
 
-console.log(productInLocalStorage.length);
 
 
-
-
-
-
-
-let buttonDelete = document.querySelectorAll('.delete');
-// Dans LocalStorage : suppression de l'article sélectionné //
-for (i=0; i < buttonDelete.length; i++) {
-  buttonDelete[i].addEventListener("click", e => {
-    
-    productInLocalStorage.splice([i],1);
-  })
+function suppr() {
+  for (let k = 0; k < productInLocalStorage.length; k++) {
+    document.querySelector(`#delete${[k]}`).addEventListener(
+      "click",
+      (ev) => {
+        productInLocalStorage.splice([k], 1);
+        console.log(productInLocalStorage);
+        localStorage.setItem("produit", JSON.stringify(productInLocalStorage));
+        document.location.reload();
+      },
+      false
+    );
+  }
 }
+
+suppr();
 
